5c0bb912fa3f7e0471555c1e1bd3eaa57f16b3b1
Premwoik <pwojtas@student.agh.edu.pl>
2019-04-23 14:08:29 +0200

    add more exact device journal logging

--- core/lib/core/device/shelly.ex
+++ core/lib/core/device/shelly.ex
@@ -3,7 +3,7 @@ defmodule Core.Device.Shelly do
 
   @behaviour Core.Device
   alias DB.{DeviceJournal}
-
+  alias Core.DeviceMonitor
 
   def start_link(host, port, opts, keywords, timeout \\ 5000, length \\ 11) do
     false
@@ -16,42 +16,73 @@ defmodule Core.Device.Shelly do
   @impl true
   def set_outputs(%{ip: ip, port: port} = device, ports) do
     port_ = List.first(ports)
-    state = if port_.state do "on" else "off" end
+    state = if port_.state, do: "on", else: "off"
     url_ = url(ip, port, port_.number)
-    resp = HTTPotion.get(url_, query: %{"turn" => state})
-    case resp.status_code do
-      200 ->
-        %{"ison" => state_} = Poison.decode! resp.body
+
+    case HTTPotion.get(url_, query: %{"turn" => state}) do
+      %HTTPotion.Response{status_code: 200, body: body} ->
+        %{"ison" => state_} = Poison.decode!(body)
+
         if state_ == port_.state do
-          DeviceJournal.log(device.id, "set_outputs", info = "Zmiana stanu portu #{port_.name} na #{state}.")
+          DeviceJournal.log(
+            device.id,
+            "set_outputs",
+            info = "Zmiana stanu portu #{port_.name} na #{state}."
+          )
+
           {:ok, []}
         else
-          DeviceJournal.log(device.id, "set_outputs", type= DeviceJournal.Type.error,
-            info = "Nie udało się zmienić stan portu #{port_.name} na #{state_}.")
+          DeviceJournal.log(
+            device.id,
+            "set_outputs",
+            type = DeviceJournal.Type.error(),
+            info = "Nie udało się zmienić stan portu #{port_.name} na #{state_}."
+          )
+
           {:error, "cannot set propper state"}
         end
-      _else -> 
-        DeviceJournal.log(device.id, "set_outputs", type= DeviceJournal.Type.error,
+
+      %HTTPotion.Response{status_code: status_code} ->
+        DeviceJournal.log(
+          device.id,
+          "set_outputs",
+          type = DeviceJournal.Type.error(),
           info = "Zły kod odpowiedzi - zmiana stanu portu #{port_.name} na #{state}.
-          Prawdopodobnie urządzenie nie posiada takiego portu.")
+          Prawdopodobnie urządzenie nie posiada takiego portu."
+        )
+
         {:error, "wrong response code"}
+
+      %HTTPotion.ErrorResponse{} ->
+        DeviceMonitor.connection_error(device, "Cannot connect to device")
+        {:error, "cannot connect to device"}
     end
   end
-  
+
   @impl true
-  def read_outputs(%{ip: ip, port: port, id: id}) do
-    #TODO change hardcoded port (pin) number
+  def read_outputs(%{ip: ip, port: port, id: id} = device) do
+    # TODO change hardcoded port (pin) number
     url_ = url(ip, port, 0)
-    resp = HTTPotion.get(url_)
-    case resp.status_code do
-      200 -> 
-        %{"ison" => state_} = Poison.decode! resp.body
+
+    case HTTPotion.get(url_) do
+      %HTTPotion.Response{status_code: 200, body: body} ->
+        %{"ison" => state_} = Poison.decode!(body)
         DeviceJournal.log(id, "read_outputs", info = "Odczytane stany #{state_}.")
         {:ok, [{0, state_}]}
-      _else -> 
-        DeviceJournal.log(id, "read_outputs", type= DeviceJournal.Type.error,
-          info = "Zły kod odpowiedzi")
+
+      %HTTPotion.Response{status_code: status_code} ->
+        DeviceJournal.log(
+          id,
+          "read_outputs",
+          type = DeviceJournal.Type.error(),
+          info = "Zły kod odpowiedzi"
+        )
+
         {:error, "wrong response code"}
-    end   
+
+      %HTTPotion.ErrorResponse{} ->
+        DeviceMonitor.connection_error(device, "Cannot connect to device")
+        {:error, "cannot connect to device"}
+    end
   end
 end
