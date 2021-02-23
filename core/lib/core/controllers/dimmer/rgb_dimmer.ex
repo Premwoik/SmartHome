4acb9564c09b16b5b8fdb031447038bb20a79b20
Premwoik <pwojtas@student.agh.edu.pl>
2021-02-24 00:24:36 +0100

    fix small bugs

--- core/lib/core/controllers/dimmer/rgb_dimmer.ex
+++ core/lib/core/controllers/dimmer/rgb_dimmer.ex
@@ -10,13 +10,15 @@ defmodule Core.Controllers.Dimmer.RgbDimmer do
   @impl true
   def set_state(dimmer, state: s) do
     Port.cast(dimmer, state: s)
+    |> List.wrap()
     |> Core.Device.do_r(:set_state)
     |> map(&Port.update/1)
   end
 
   @impl true
-  def set_brightness(dimmer, fill) do
+  def set_brightness(dimmer, fill: fill) do
     Port.cast(dimmer, fill: fill)
+    |> List.wrap()
     |> Core.Device.do_r(:set_brightness)
     |> map(&Port.update/1)
   end
@@ -24,6 +26,7 @@ defmodule Core.Controllers.Dimmer.RgbDimmer do
   @impl true
   def set_color(dimmer, red: r, green: g, blue: b) do
     Port.cast(dimmer, more: [red: r, green: g, blue: b])
+    |> List.wrap()
     |> Core.Device.do_r(:set_color)
     |> map(&Port.update/1)
   end
@@ -31,17 +34,11 @@ defmodule Core.Controllers.Dimmer.RgbDimmer do
   @impl true
   def set_white_brightness(dimmer, fill: fill) do
     Port.cast(dimmer, more: [white: fill])
+    |> List.wrap()
     |> Core.Device.do_r(:set_white_brightness)
     |> map(&Port.update/1)
   end
 
   # Privates
 
-  #  defp process_response(response) do
-  #    with {:ok, dimmer} <- response do
-  #      dimmer = Port.update(dimmer)
-  #      Channel.broadcast_item_change("dimmer", dimmer.id, dimmer.ref)
-  #      :ok
-  #    end
-  #  end
 end
