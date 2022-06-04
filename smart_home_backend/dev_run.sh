a5eaef2fb5fc2dbfd72b3165d65b4bb22883ada0
Przemysław Wojtasik <przemyslaw.wojtasik@erlang-solutions.com>
2022-06-05 00:40:44 +0200

    Allow run full Core app on dev machine and update basement_core

--- smart_home_backend/dev_run.sh
+++ smart_home_backend/dev_run.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 export LC_ALL=en_GB.UTF-8
 export LC_CTYPE=en_GB.UTF-8
-TARGET=mac iex --name sh@192.168.2.112 --cookie COOKIE -S mix phx.server
+
+if ! [[ $1 = "prod" ]] ; then
+export TARGET=mac
+fi
+
+iex --name sh@192.168.2.112 --cookie COOKIE -S mix phx.server
 
