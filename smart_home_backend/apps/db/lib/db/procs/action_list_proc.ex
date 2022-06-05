515902fed2f8fffac0a2b636facd6a7aebe0b626
Przemysław Wojtasik <przemyslaw.wojtasik@erlang-solutions.com>
2022-06-05 12:06:28 +0200

    Refactor ActionListProc and user ProcBeh

--- smart_home_backend/apps/db/lib/db/procs/action_list_proc.ex
+++ smart_home_backend/apps/db/lib/db/procs/action_list_proc.ex
@@ -5,35 +5,93 @@ defmodule DB.Proc.ActionListProc do
 
   alias DB.Data.Action
   alias DB.Data.Port
+  alias DB.Proc.Beh, as: ProcBeh
 
+  @behaviour ProcBeh
+
+  @type resp() :: ProcBeh.resp(Action.t())
+  @type resp!() :: ProcBeh.resp!(Action.t())
+  @type id() :: ProcBeh.item_id()
+
+  @type state_t() :: %{actions: [Action]}
+
+  ## API
+
+  @impl ProcBeh
+  @doc ""
   def start_link(_opts) do
     GenServer.start_link(__MODULE__, [], name: __MODULE__)
   end
 
-  @spec add(map() | Action.t()) :: {:ok, Action.t()} | {:error, Ecto.Changeset.t()}
-  def add(params) do
-    GenServer.call(__MODULE__, {:insert, params})
-  end
-
-  @spec update(integer(), map() | Action.t()) :: {:ok, Action.t()} | {:error, Ecto.Changeset.t()}
+  @impl ProcBeh
+  @doc ""
+  @spec update(id(), map() | Action.t()) :: resp()
   def update(action_id, params) do
     params = Map.delete(params, :__struct__)
     GenServer.call(__MODULE__, {:update, action_id, params})
   end
 
-  @spec fast_update(integer(), map() | Action.t()) ::
-          {:ok, Action.t()} | {:error, Ecto.Changeset.t()}
+  @impl ProcBeh
+  @doc ""
+  @spec update!(id(), map() | Action.t()) :: resp!()
+  def update!(action_id, params) do
+    ProcBeh.get_just(update(action_id, params))
+  end
+
+  @impl ProcBeh
+  @doc ""
+  @spec fast_update(id(), map() | Action.t()) :: resp()
   def fast_update(action_id, params) do
     params = Map.delete(params, :__struct__)
     GenServer.call(__MODULE__, {:not_persistent_update, action_id, params})
   end
 
-  @spec get!(integer()) :: Action.t() | nil
+  @impl ProcBeh
+  @doc ""
+  @spec fast_update!(id(), map() | Action.t()) :: resp!()
+  def fast_update!(action_id, params) do
+    ProcBeh.get_just(fast_update(action_id, params))
+  end
+
+  @impl ProcBeh
+  @doc ""
+  @spec get!(id()) :: resp!()
   def get!(action_id) do
-    case get(action_id) do
-      {:ok, action} -> action
-      nil -> nil
-    end
+    ProcBeh.get_just(get(action_id))
+  end
+
+  @impl ProcBeh
+  @doc ""
+  @spec get(id()) :: resp()
+  def get(action_id) do
+    GenServer.call(__MODULE__, {:get, action_id})
+  end
+
+  @impl ProcBeh
+  @doc ""
+  @spec list_all() :: {:ok, [Action.t()]}
+  def list_all() do
+    GenServer.call(__MODULE__, :all)
+  end
+
+  @impl ProcBeh
+  @doc ""
+  @spec list_all!() :: [Action.t()]
+  def list_all!() do
+    ProcBeh.get_just(list_all!(), [])
+  end
+
+  @impl ProcBeh
+  @doc ""
+  @spec force_read_all() :: :ok
+  def force_read_all() do
+    GenServer.cast(__MODULE__, {:force_read, :all})
+  end
+
+  ## Custom API
+
+  def force_read_id(id) do
+    GenServer.cast(__MODULE__, {:force_read, {:id, id}})
   end
 
   @spec get_ids([integer()]) :: [Action.t()]
@@ -41,9 +99,9 @@ defmodule DB.Proc.ActionListProc do
     GenServer.call(__MODULE__, {:get_ids, action_ids})
   end
 
-  @spec get(integer()) :: {:ok, Action.t()} | {:error, term()}
-  def get(action_id) do
-    GenServer.call(__MODULE__, {:get, action_id})
+  @spec add(map() | Action.t()) :: {:ok, Action.t()} | {:error, Ecto.Changeset.t()}
+  def add(params) do
+    GenServer.call(__MODULE__, {:insert, params})
   end
 
   @spec list_active() :: {:ok, [Action.t()]} | {:error, term()}
@@ -53,57 +111,31 @@ defmodule DB.Proc.ActionListProc do
 
   @spec get_by_activator([Port.t()]) :: {:ok, [Action.t()]} | {:error, term()}
   def get_by_activator(ports) do
-    GenServer.call(__MODULE__, {:get_by_activator, ports})
+    ports_ids = Enum.map(ports, & &1.id)
+    GenServer.call(__MODULE__, {:get_by_activator, ports_ids})
   end
 
-  @spec list_all() :: {:ok, [Action.t()]}
-  def list_all() do
-    GenServer.call(__MODULE__, :all)
-  end
+  ## GenServer API
 
-  @spec list_all!() :: {:ok, [Action.t()]}
-  def list_all!() do
-    {:ok, res} = GenServer.call(__MODULE__, :all)
-    res
-  end
-
-  def force_read_all() do
-    GenServer.cast(__MODULE__, {:force_read, :all})
-  end
-
-  def force_read_id(id) do
-    GenServer.cast(__MODULE__, {:force_read, {:id, id}})
-  end
-
-  @type state_t() :: %{actions: [Action]}
-
-  @impl true
+  @impl GenServer
   def init(_init_arg) do
-    actions = read_all_from_db()
-    {:ok, %{actions: actions}}
+    {:ok, %{actions: read_all_from_db()}}
   end
 
-  @impl true
+  @impl GenServer
   def handle_call(:all, _from, %{actions: actions} = state) do
-    return = Enum.map(actions, fn {_k, v} -> v end)
-    {:reply, {:ok, return}, state}
+    {:reply, {:ok, Map.values(actions)}, state}
   end
 
-  @impl true
+  @impl GenServer
   def handle_call(:all_active, _from, %{actions: actions} = state) do
-    return = Enum.filter(actions, fn {_k, v} -> v.state end) |> Enum.map(fn {_k, v} -> v end)
+    return = Map.values(actions) |> Enum.filter(fn a -> a.state end)
     {:reply, {:ok, return}, state}
   end
 
-  @impl true
-  def handle_call({:get, id}, _from, %{actions: actions} = state) do
-    return =
-      case Map.get(actions, id) do
-        nil -> {:error, :not_found}
-        action -> {:ok, action}
-      end
-
-    {:reply, return, state}
+  @impl GenServer
+  def handle_call({:get, id}, _from, state) do
+    {:reply, get_action(state, id), state}
   end
 
   @impl GenServer
@@ -111,10 +143,8 @@ defmodule DB.Proc.ActionListProc do
     {:reply, Map.values(Map.take(actions, ids)), state}
   end
 
-  @impl true
-  def handle_call({:get_by_activator, ports}, _from, %{actions: actions_map} = state) do
-    ports_ids = Enum.map(ports, & &1.id)
-
+  @impl GenServer
+  def handle_call({:get_by_activator, ports_ids}, _from, %{actions: actions_map} = state) do
     return =
       actions_map
       |> Map.values()
@@ -127,7 +157,7 @@ defmodule DB.Proc.ActionListProc do
     {:reply, return, state}
   end
 
-  @impl true
+  @impl GenServer
   def handle_call({:update, action_id, params}, _from, state) do
     with {:ok, action} <- get_action(state, action_id),
          {:ok, updated_action} <- Action.update(action, params) do
@@ -139,7 +169,7 @@ defmodule DB.Proc.ActionListProc do
     end
   end
 
-  @impl true
+  @impl GenServer
   def handle_call({:not_persistent_update, action_id, params}, _from, state) do
     with {:ok, action} <- get_action(state, action_id),
          {:ok, updated_action} <- Action.virtual_update(action, params) do
@@ -151,7 +181,7 @@ defmodule DB.Proc.ActionListProc do
     end
   end
 
-  @impl true
+  @impl GenServer
   def handle_call({:insert, params}, _from, state) do
     with {:ok, action} <- Action.insert(params) do
       {:reply, {:ok, action}, put_action(state, action)}
@@ -161,23 +191,21 @@ defmodule DB.Proc.ActionListProc do
     end
   end
 
-  @impl true
+  @impl GenServer
   def handle_cast({:force_read, :all}, state) do
-    actions = read_all_from_db()
-    {:noreply, %{state | actions: actions}}
+    {:noreply, %{state | actions: read_all_from_db()}}
   end
 
-  @impl true
+  @impl GenServer
   def handle_cast({:force_read, {:id, id}}, state) do
     {:ok, action} = Map.fetch(read_all_from_db(), id)
     {:noreply, put_action(state, action)}
   end
 
+  ## Internal helpers
+
   defp get_action(%{actions: actions}, action_id) do
-    case Map.get(actions, action_id) do
-      nil -> {:error, :not_found}
-      action -> {:ok, action}
-    end
+    ProcBeh.get_maybe(actions[action_id])
   end
 
   defp put_action(%{actions: actions} = state, action) do
